8d55cc3e81063cd5d446d62150fe674c30f51666
GvMazzon25 <gvmazzon25@gmail.com>
2022-01-22 12:29:18 +0100

    new-route-start

---
+++ resources/views/partials/nav.blade.php
@@ -0,0 +1,9 @@
+<div class="container text-center">
+    <a href="{{ route('home') }}">
+        <h1>HomePage</h1>
+    </a>
+    <ul>
+        <li><a href="{{ route('movies') }}">Movies</a></li>
+        <li><a href=""></a>News</li>
+    </ul>
+</div>
