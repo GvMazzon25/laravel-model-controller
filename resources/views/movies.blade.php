825b931dad9d9f3191d6774c35df7bb51dda833d
GvMazzon25 <gvmazzon25@gmail.com>
2022-01-22 16:18:43 +0100

    movies

--- resources/views/movies.blade.php
+++ resources/views/movies.blade.php
@@ -13,7 +13,16 @@
     </nav>
     <main class="text-center">
         <div class="container">
-            
+            <div class="card" style="width: 18rem;">
+                <div class="card-header">
+                  Featured
+                </div>
+                <ul class="list-group list-group-flush">
+                  <li class="list-group-item">An item</li>
+                  <li class="list-group-item">A second item</li>
+                  <li class="list-group-item">A third item</li>
+                </ul>
+              </div>
         </div>
     </main>
 </body>
